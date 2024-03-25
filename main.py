b2900de3ef09e1a957d5851cd42f756aca538483
Mariia Iermolenko <iermolenko.mariia@gmail.com>
2024-03-25 22:55:00 +0200

    add enemies

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import random
 import pygame
-from pygame.constants import QUIT
+from pygame.constants import QUIT, K_DOWN, K_UP, K_RIGHT, K_LEFT
 
 pygame.init()
 
@@ -10,14 +11,32 @@ HEIGHT = 600
 WIDTH = 1000
 COLOR_BLACK = (0, 0, 0)
 COLOR_WHITE = (255, 255, 255)
+COLOR_BLUE = (0, 0, 255)
 
 main_display = pygame.display.set_mode((WIDTH, HEIGHT))
 
-player_size = (20, 20)
-player = pygame.Surface(player_size)
+PLAYER_SIZE = (20, 20)
+player = pygame.Surface(PLAYER_SIZE)
 player.fill(COLOR_WHITE)
 player_rect = player.get_rect()
-player_direction = [1, 1]
+MOVE_DOWN = [0, 1]
+MOVE_UP = [0, -1]
+MOVE_RIGHT = [1, 0]
+MOVE_LEFT = [-1, 0]
+
+ENEMY_SIZE = (20, 20)
+enemy_surface = pygame.Surface(ENEMY_SIZE)
+enemy_surface.fill(COLOR_BLUE)
+
+def create_enemy(size):
+    rect = pygame.Rect(WIDTH, random.randint(0, HEIGHT), *size)
+    move = [random.randint(-6, -1), 0]
+    return [rect, move]
+
+CREATE_ENEMY = pygame.USEREVENT + 1
+pygame.time.set_timer(CREATE_ENEMY, 1500)
+
+enemies = []
 
 playing = True
 
@@ -27,23 +46,33 @@ while playing:
     for event in pygame.event.get():
         if event.type == QUIT:
             playing = False
+        if event.type == CREATE_ENEMY:
+            enemies.append(create_enemy(ENEMY_SIZE))
 
     main_display.fill(COLOR_BLACK)
 
-    if player_rect.bottom >= HEIGHT:
-        player_direction = [1, -1]
+    keys = pygame.key.get_pressed()
+
+    if keys[K_DOWN] and player_rect.bottom < HEIGHT:
+        player_rect = player_rect.move(MOVE_DOWN)
     
-    if player_rect.top < 0:
-        player_direction = [-1, 1]
+    if keys[K_UP] and player_rect.top > 0:
+        player_rect = player_rect.move(MOVE_UP)
     
-    if player_rect.right >= WIDTH:
-        player_direction = [-1, -1]
+    if keys[K_RIGHT] and player_rect.right < WIDTH:
+        player_rect = player_rect.move(MOVE_RIGHT)
     
-    if player_rect.left < 0:
-        player_direction = [1, 1]
+    if keys[K_LEFT] and player_rect.left > 0:
+        player_rect = player_rect.move(MOVE_LEFT)
+    
+    for enemy in enemies:
+        enemy[0] = enemy[0].move(enemy[1])
+        main_display.blit(enemy_surface, enemy[0])
     
     main_display.blit(player, player_rect)
 
-    player_rect = player_rect.move(player_direction)
-
     pygame.display.flip()
+
+    for enemy in enemies:
+        if enemy[0].left < 0:
+            enemies.pop(enemies.index(enemy))
